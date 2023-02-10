8ad07a36e4f9a1353c1898bf53c4d4ef2f713aa0
AidanMiddel <Aidanm@ctjies.nl>
2023-02-10 03:28:38 +0100

    video 5

--- src/Pages/Productpage.js
+++ src/Pages/Productpage.js
@@ -1,15 +1,23 @@
 import itemsObject from "../Data/items"
+import "./Productpage.css";
 
 const Productpage = () => {
     const toBeRenderd = itemsObject[0].map(item => {
         return (
-            <h1>{item.name}</h1>
+            <li key={item.name} className="productpage__listitem">
+                <img src={item.img} alt={item.alt} />
+                <p>{item.name}</p>
+                <p>{item.description}</p>
+                <button>add to card</button>
+            </li>
         )
     })
     return (
-        <>
-            {toBeRenderd}
-        </>
+        <article className="productpage">
+            <ul className="productpage__list">
+                {toBeRenderd}
+            </ul>
+        </article>
     )
 }
 
--- videos.md
+++ videos.md
@@ -1,4 +1,3 @@
-Video 5/8 => https://web.microsoftstream.com/video/912cc622-be5f-45b1-a7ee-ff4b6fc8e0d2
 Video 6/8 => https://web.microsoftstream.com/video/5db32ef1-2abf-4385-945a-39d8e5612b33 
 Video 7/8 => https://web.microsoftstream.com/video/8acafe6d-c383-4a4a-8c1a-47177c0dbd50
 Video 8/8 => https://web.microsoftstream.com/video/33008ebe-14d8-476b-b3e5-3a6d6453b147
